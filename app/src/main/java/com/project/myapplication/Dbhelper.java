bcbed714c50a622007f05597cee98a2ae5fb20f2
N3by <cristian.alvarezp@uniagustiniana.edu.co>
2023-03-13 23:13:56 -0500

    Solucion error de version de java 1.8 a Java 11, que pena profe ahora si compila :)

--- app/src/main/java/com/project/myapplication/Dbhelper.java
+++ app/src/main/java/com/project/myapplication/Dbhelper.java
@@ -1,9 +1,11 @@
 package com.project.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class Dbhelper extends SQLiteOpenHelper {
 
@@ -11,17 +13,28 @@ public class Dbhelper extends SQLiteOpenHelper {
     private static final String DATABASE_NOMBRE = "amigos.db";
     public static final String TABLE_AMIGOS = "amigos";
 
+    private static final String COLUMN_ID = "_id";
+
+    private static final String COLUMN_NAME =  "nombre";
+
+    private static final String COLUMN_AGE =  "edad";
+
+    private static final String COLUMN_CIUDAD =  "ciudad";
+    private final Context context;
+
+
     public Dbhelper(@Nullable Context context) {
         super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_AMIGOS + "(" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "nombre TEXT NOT NULL," +
-                "edad TEXT NOT NULL," +
-                "ciudad TEXT)");
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME + " TEXT NOT NULL," +
+                COLUMN_AGE + " TEXT NOT NULL," +
+                COLUMN_CIUDAD + "TEXT)");
     }
 
     @Override
@@ -30,5 +43,32 @@ public class Dbhelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE " + TABLE_AMIGOS);
         onCreate(sqLiteDatabase);
 
+    }
+
+    void addFriend(String nombre, String edad, String ciudad){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_NAME, nombre);
+        cv.put(COLUMN_AGE, ageFactorial(edad));
+        cv.put(COLUMN_CIUDAD, ciudad);
+
+
+        long result  = db.insert(TABLE_AMIGOS, null, cv);
+        if(result == -1){
+
+            Toast.makeText(context, "Error agregando amigo.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "amigo agregado exitosamente.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static int ageFactorial(String edad){
+        int num = Integer.parseInt(edad);
+        if (num == 0){
+            return 1;
+        }
+        else {
+            return num * ageFactorial(Integer.toString(num-1));
+        }
     }
 }
--- app/src/main/java/com/project/myapplication/addFriendActivity.java
+++ app/src/main/java/com/project/myapplication/addFriendActivity.java
@@ -2,15 +2,49 @@ package com.project.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class addFriendActivity extends AppCompatActivity {
+
+    EditText friendName, friendAge, friendCity;
+    Button storeFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
-        Dbhelper dbhelper = new Dbhelper(addFriendActivity.this);
 
+        Toast.makeText(this, "Se cambio de pantalla exitosamente", Toast.LENGTH_SHORT).show();
+
+        //conecto lo visual con la logica
+
+        //EditText
+        friendName = findViewById(R.id.friendName);
+        friendAge = findViewById(R.id.friendAge);
+        friendCity = findViewById(R.id.friendCity);
+
+        //botones
+        storeFriend = findViewById(R.id.storeFriend);
+        //
+        //onclicklistener
+        storeFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Dbhelper dbhelper = new Dbhelper(addFriendActivity.this);
+                dbhelper.addFriend(
+                        friendName.getText().toString().trim(),
+                        friendAge.getText().toString().trim(),
+                        friendCity.getText().toString().trim()
+                );
+            }
+        });
+        //1
 
     }
+
+
 }
